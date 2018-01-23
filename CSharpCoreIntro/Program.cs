f4e69e08563cfd2d6bd665742cb1e980679b3834
Faithe Olson <faithe.olson@gmail.com>
2018-01-23 12:44:31 -0600

    This is the first commit

---
+++ CSharpCoreIntro/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCoreIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // variables
+            double input;
+
+            // input
+            Console.WriteLine("Please enter a number! Like now!");
+            input = double.Parse(Console.ReadLine());
+
+            // processing
+            input = Math.Sqrt(input);
+
+            // output
+
+            //Console.WriteLine("The output is {0}.", input);
+            Console.WriteLine($"The output is {input}");
+
+        
+
+
+        }
+    }
+}
